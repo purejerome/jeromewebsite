958b313ea466d3333dd80d49dd0dd416e72bbe30
Jerome <donfacjc@dukes.jmu.edu>
2024-09-28 18:03:29 -0400

    just fucking print the console path so i can see somethign please

--- scripts/titan.js
+++ scripts/titan.js
@@ -13,127 +13,127 @@
 
 console.log(window.location.pathname);
 
-import * as THREE from "../node_modules/three";
-import { GLTFLoader } from "../node_modules/three/examples/jsm/loaders/GLTFLoader.js";
-import ScrollTrigger from "../node_modules/gsap/scrolltrigger";
-import gsap from "../node_modules/gsap";
-import { CustomEase } from "../node_modules/gsap/CustomEase";
+// import * as THREE from "../node_modules/three";
+// import { GLTFLoader } from "../node_modules/three/examples/jsm/loaders/GLTFLoader.js";
+// import ScrollTrigger from "../node_modules/gsap/scrolltrigger";
+// import gsap from "../node_modules/gsap";
+// import { CustomEase } from "../node_modules/gsap/CustomEase";
 
-gsap.registerPlugin(CustomEase);
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(CustomEase);
+// gsap.registerPlugin(ScrollTrigger);
 
-// Scene
-const scene = new THREE.Scene();
+// // Scene
+// const scene = new THREE.Scene();
 
-// Camera
-const cam = new THREE.PerspectiveCamera(
-  75,
-  innerWidth / innerHeight,
-  0.1,
-  1000
-);
-cam.position.set(0.09521213829704991, 0.46850471403480043, 15.529081514766155);
+// // Camera
+// const cam = new THREE.PerspectiveCamera(
+//   75,
+//   innerWidth / innerHeight,
+//   0.1,
+//   1000
+// );
+// cam.position.set(0.09521213829704991, 0.46850471403480043, 15.529081514766155);
 
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#bg"),
-  alpha: true,
-});
-renderer.setPixelRatio(devicePixelRatio);
-renderer.setSize(innerWidth, innerHeight);
+// // Renderer
+// const renderer = new THREE.WebGLRenderer({
+//   canvas: document.querySelector("#bg"),
+//   alpha: true,
+// });
+// renderer.setPixelRatio(devicePixelRatio);
+// renderer.setSize(innerWidth, innerHeight);
 
-// Handle window resize
-window.addEventListener("resize", () => {
-  renderer.setSize(innerWidth, innerHeight);
-  cam.aspect = innerWidth / innerHeight;
-  cam.updateProjectionMatrix();
-});
+// // Handle window resize
+// window.addEventListener("resize", () => {
+//   renderer.setSize(innerWidth, innerHeight);
+//   cam.aspect = innerWidth / innerHeight;
+//   cam.updateProjectionMatrix();
+// });
 
-let titan;
+// let titan;
 
-const gltfLoader = new GLTFLoader();
+// const gltfLoader = new GLTFLoader();
 
-let titanPos;
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#bg",
-    scrub: 2,
-    start: "top top",
-    end: "bottom top",
-  },
-});
-let lookAT;
+// let titanPos;
+// let tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: "#bg",
+//     scrub: 2,
+//     start: "top top",
+//     end: "bottom top",
+//   },
+// });
+// let lookAT;
 
-gltfLoader.load(
-  "models/titan/ColosalTitan.glb",
-  (gltf) => {
-    titan = gltf.scene;
-    titan.scale.set(10, 10, 10);
-    const titanBox = new THREE.Box3().setFromObject(titan); // Get bounding box of Titan model
-    const titanTop = titanBox.max.y;
-    titanPos = new THREE.Vector3();
-    titanPos.copy(titan.position);
-    titanPos.y = titanTop - 10;
-    scene.add(titan);
-    lookAT = titanPos;
-    cam.lookAt(titanPos);
+// gltfLoader.load(
+//   "models/titan/ColosalTitan.glb",
+//   (gltf) => {
+//     titan = gltf.scene;
+//     titan.scale.set(10, 10, 10);
+//     const titanBox = new THREE.Box3().setFromObject(titan); // Get bounding box of Titan model
+//     const titanTop = titanBox.max.y;
+//     titanPos = new THREE.Vector3();
+//     titanPos.copy(titan.position);
+//     titanPos.y = titanTop - 10;
+//     scene.add(titan);
+//     lookAT = titanPos;
+//     cam.lookAt(titanPos);
 
-    tl.to(titan.rotation, {
-      y: Math.PI * 2,
-      duration: 20,
-    })
-      .to(
-        cam.position,
-        {
-          y: cam.position.y + 30,
-          x: cam.position.x - 5,
-          duration: 20,
-        },
-        "<"
-      )
-      .to(
-        lookAT,
-        {
-          y: lookAT.y + 100,
-          duration: 20,
-          onUpdate: () => {
-            cam.lookAt(lookAT);
-          },
-        },
-        "<"
-      );
-  },
-  undefined,
-  function (error) {
-    console.error(error);
-  }
-);
+//     tl.to(titan.rotation, {
+//       y: Math.PI * 2,
+//       duration: 20,
+//     })
+//       .to(
+//         cam.position,
+//         {
+//           y: cam.position.y + 30,
+//           x: cam.position.x - 5,
+//           duration: 20,
+//         },
+//         "<"
+//       )
+//       .to(
+//         lookAT,
+//         {
+//           y: lookAT.y + 100,
+//           duration: 20,
+//           onUpdate: () => {
+//             cam.lookAt(lookAT);
+//           },
+//         },
+//         "<"
+//       );
+//   },
+//   undefined,
+//   function (error) {
+//     console.error(error);
+//   }
+// );
 
-const pointLight = new THREE.PointLight(0xffffff, 100);
-// pointLight.position.set(0, 5, 10);
-scene.add(pointLight);
+// const pointLight = new THREE.PointLight(0xffffff, 100);
+// // pointLight.position.set(0, 5, 10);
+// scene.add(pointLight);
 
-// "amb lighting"
+// // "amb lighting"
 
-const aLight = new THREE.PointLight(0xffffff, 20000);
-aLight.position.set(0, 5, 100);
-scene.add(aLight);
+// const aLight = new THREE.PointLight(0xffffff, 20000);
+// aLight.position.set(0, 5, 100);
+// scene.add(aLight);
 
-// Animation loop
-function animate() {
-  requestAnimationFrame(animate);
-  const time = performance.now() * 0.0005;
-  const radius = 10;
+// // Animation loop
+// function animate() {
+//   requestAnimationFrame(animate);
+//   const time = performance.now() * 0.0005;
+//   const radius = 10;
 
-  pointLight.position.x = Math.sin(time) * radius;
-  pointLight.position.z = Math.cos(time) * radius;
-  pointLight.position.y = 10;
-  if (pointLight.position.z < -2) {
-    pointLight.intensity = 500;
-  } else {
-    pointLight.intensity = 100;
-  }
-  renderer.render(scene, cam);
-}
+//   pointLight.position.x = Math.sin(time) * radius;
+//   pointLight.position.z = Math.cos(time) * radius;
+//   pointLight.position.y = 10;
+//   if (pointLight.position.z < -2) {
+//     pointLight.intensity = 500;
+//   } else {
+//     pointLight.intensity = 100;
+//   }
+//   renderer.render(scene, cam);
+// }
 
-animate();
+// animate();
