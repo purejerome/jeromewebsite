0aa9e48fe61b89fa43e6dba3d84810533f4e358b
Jerome <donfacjc@dukes.jmu.edu>
2024-09-28 21:05:58 -0400

    ?

--- scripts/titan.js
+++ scripts/titan.js
@@ -1,5 +1,5 @@
-import * as THREE from "../three.js-master/build/three.module.js";
-import { GLTFLoader } from "../three.js-master/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "./three.js-master/build/three.module.js";
+import { GLTFLoader } from "./three.js-master/examples/jsm/loaders/GLTFLoader.js";
 // import gsap from "../gsap-public/";
 // import gsap from "gsap";
 // import ScrollTrigger from "gsap/ScrollTrigger.js";
